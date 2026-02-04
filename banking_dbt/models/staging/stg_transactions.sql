62df1b960ace6cd8879fb16748dbcd612ca75a53
William <williamjonson077@gmail.com>
2026-02-04 12:59:06 +0530

    Add deduplication logic to stg_transactions

--- banking_dbt/models/staging/stg_transactions.sql
+++ banking_dbt/models/staging/stg_transactions.sql
@@ -1,10 +1,37 @@
+with base as (
+
+    SELECT
+        v:id::string                 AS transaction_id,
+        v:account_id::string         AS account_id,
+        v:amount::float              AS amount,
+        v:txn_type::string           AS transaction_type,
+        v:related_account_id::string AS related_account_id,
+        v:status::string             AS status,
+        v:created_at::timestamp      AS transaction_time,
+        CURRENT_TIMESTAMP            AS load_timestamp
+    FROM {{ source('raw', 'transactions') }}
+
+),
+
+deduped as (
+
+    SELECT *,
+           ROW_NUMBER() OVER (
+               PARTITION BY transaction_id
+               ORDER BY transaction_time DESC
+           ) AS rn
+    FROM base
+
+)
+
 SELECT
-    v:id::string                 AS transaction_id,
-    v:account_id::string         AS account_id,
-    v:amount::float              AS amount,
-    v:txn_type::string           AS transaction_type,
-    v:related_account_id::string AS related_account_id,
-    v:status::string             AS status,
-    v:created_at::timestamp      AS transaction_time,
-    CURRENT_TIMESTAMP            AS load_timestamp
-FROM {{ source('raw', 'transactions') }}
+    transaction_id,
+    account_id,
+    amount,
+    transaction_type,
+    related_account_id,
+    status,
+    transaction_time,
+    load_timestamp
+FROM deduped
+WHERE rn = 1
